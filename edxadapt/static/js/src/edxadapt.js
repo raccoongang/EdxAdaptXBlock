b561662716799d74977ab805869baa9a2c2a0b62
Igor Degtiarov <igoryok.d@gmail.com>
2017-01-17 19:18:55 +0200

    Emit event after successful student enrollment in EdxAdapt
    Event passes url to the first first problem to solve and is used to enable "next" buttons.

--- edxadapt/static/js/src/edxadapt.js
+++ edxadapt/static/js/src/edxadapt.js
@@ -9,7 +9,7 @@ function EdxAdaptXBlock(runtime, element) {
     var skills = options.data('skills');
 
     /** Create user */
-    var registerUser = function(){
+    function registerUser() {
         $.ajax({
             url: apiBaseUrl + '/course/' + courseId + '/user',
             type: 'POST',
@@ -58,7 +58,7 @@ function EdxAdaptXBlock(runtime, element) {
         })
         .done(function(data, textStatus) {
             console.log("success", data, textStatus);
-            setStatusOk();
+            setStatusOk(data);
         })
         .fail(function(jqXHR, textStatus) {
             if (jqXHR.status == 404) {
@@ -68,9 +68,60 @@ function EdxAdaptXBlock(runtime, element) {
             console.log("Failed to register student in EdxAdapt", jqXHR.responseText);
         });
     }
-    function setStatusOk() {
-        $('#status_ok').css('visibility', 'visible');
+
+    /**
+    * Create and publish custom event with url to the problem chosen by EdxAdapt.
+    * @param {object} data - response data from EdxAdapt
+    */
+    function createEnrollOkEvent(data) {
+        var problemUrl;
+        if (data.current) {
+            problemUrl = data.current.tutor_url;
+        } else if (data.next) {
+            problemUrl = data.next.tutor_url;
+        }
+        if (problemUrl) {
+            var event = new CustomEvent(
+                "edxAdaptEnrollOk", {detail: {adaptiveProblem: problemUrl}}
+            );
+            document.dispatchEvent(event);
+        } else {
+            console.log("Failed to get tutor_url to the first adaptive problem");
+        }
     }
+
+    /**
+    * Get student's status data form the EdxAdapt and use it for custom event creation.
+    * @param {object} data - response data from EdxAdapt
+    */
+   function getFirstAdaptiveProblem(data) {
+        if (data) {
+            createEnrollOkEvent(data);
+        } else {
+            $.ajax({
+                url: apiBaseUrl + '/course/' + courseId + '/user/' + studentId,
+                type: 'GET',
+                contentType: 'application/json',
+            })
+            .done(function(data, textStatus) {
+                console.log("First problem successfully found", textStatus);
+                createEnrollOkEvent(data);
+            })
+            .fail(function(jqXHR, textStatus) {
+                console.log("Failed to get first adaptive problem for student from EdxAdapt", jqXHR.responseText);
+            });
+        }
+    }
+
+    /**
+    * Set visible #statusOk div and create custom event student enrolled successful.
+    * @param {object} data - response data from EdxAdapt
+    */
+    function setStatusOk(data) {
+        $('#status_ok').css('visibility', 'visible');
+        getFirstAdaptiveProblem(data);
+    }
+
     function setStatusNok() {
         $('#status_nok').css('visibility', 'visible');
     }
